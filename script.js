cc19b52a407b8381cf7a889bc39f9c9d0cd0a82d
Shifatur-Rahman <rjnovel28@gmail.com>
2022-05-29 05:39:24 +0600

    new file add

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-let arr = [15, 19, 9, 25, 75, 11];
-document.write('Array is : '+ arr);
-let max = arr[0];
+// let arr = [15, 19, 9, 25, 75, 11];
+// document.write('Array is : '+ arr);
+// let max = arr[0];
 
-for(let i=0; i<arr.length; i++){
+// for(let i=0; i<arr.length; i++){
 
-    if(arr[i]>max){
-        max = arr[i];
-    }
+//     if(arr[i]>max){
+//         max = arr[i];
+//     }
+// }
+
+// document.getElementById('para').innerHTML = max;
+
+let size = prompt('Enter your array limit');
+let i, myInputArr = [], max;
+for(i=0; i<size; i++){
+   myInputArr[i] = prompt('Enter your Number');
 }
 
-document.getElementById('para').innerHTML = max;
+max = myInputArr[0];
+
+for(i=0; i<size; i++){
+    if(myInputArr[i] > max){
+        max = myInputArr[i];
+     }
+}
+
+document.write('Array: '+myInputArr+ '<br/>');
+document.write('The Largest Number is = ' + max);
+
+// console.log(myInputArr);
+
+
+
+
+
+
