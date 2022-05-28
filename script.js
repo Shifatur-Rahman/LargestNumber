d239b8c18991f5a3cfab5a8285a3519fbca1e387
Shifatur-Rahman <rjnovel28@gmail.com>
2022-05-29 00:19:39 +0600

    script add

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let arr = [15, 19, 9, 25, 75, 11];
-
+document.write('Array is : '+ arr);
 let max = arr[0];
 
 for(let i=0; i<arr.length; i++){
@@ -7,7 +7,6 @@ for(let i=0; i<arr.length; i++){
     if(arr[i]>max){
         max = arr[i];
     }
-
 }
 
 document.getElementById('para').innerHTML = max;
