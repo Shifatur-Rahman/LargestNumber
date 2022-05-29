d72674d59e6bff4015695f0e32462bdf27fca0b4
Shifatur-Rahman <59673387+Shifatur-Rahman@users.noreply.github.com>
2022-05-29 11:12:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 
 let size = prompt('Enter your array limit');
-let i, myInputArr = [], max;
+let i, myInputArr = [], max=0;
 for(i=0; i<size; i++){
-   myInputArr[i] = parseInt(prompt('Enter your Number'));
-}
-
-max = myInputArr[0];
-
-for(i=0; i<size; i++){
-    if(myInputArr[i] > max){
+        myInputArr[i] = parseInt(prompt('Enter your Number'));
+        if(myInputArr[i] > max){
         max = myInputArr[i];
      }
 }
+
 document .write('Your Array Limit = '+ size + '<br/>');
 document.write('Array = '+myInputArr+ '<br/>');
 document.write('The Largest Number is = ' + max);
-
-
-
-
-
-
